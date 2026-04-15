ece6534f71a784c87a58daee5fba5e4ea4acc0aa
Filipe Lima <filipe.slima2@ufpe.br>
2026-04-15 18:19:44 -0300

    Create topicsbtn.js

---
+++ assets/js/topicsbtn.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById("topics-toggle");
+  if (!btn) return;
+
+  btn.addEventListener("click", function() {
+    document.body.classList.toggle("sidebar-open");
+  });
+});
